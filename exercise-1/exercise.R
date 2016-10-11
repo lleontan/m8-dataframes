3c4f565594268ca749ab72d22bfa37af6ec9c1ec
lleontan <lleontan@gmail.com>
2016-10-11 14:21:19 -0700

    I changed some things

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,25 +2,27 @@
 
 
 # Create a vector of everything you ate for breakfast
-
+breakfast<-c("coffee","bacon","eggs","toast")
 
 # Create a vector of everything you ate for lunch
-
+lunch<-c("chicken","rice","sugar water")
 
 # Create a list `meals` that has contains your breakfast and lunch
-
+meals<- list('breakfast'=breakfast,'lunch'=lunch)
 
 # Add a `dinner` index to your `meals` list that has what you plan to eat for dinner
-
+meals$dinner<-c("rice","ramen","fancyBread")
 
 # Extract your 'dinner' element from your list and save it in a vector called 'dinner'
-
+dinner=meals[["dinner"]]
 
 ### Bonus ### 
 # Create a list that has the number of items you ate for each meal
-
+mealCount<-list(lengths(meals))
 
 # Write a function that adds pizza to every meal
-
-
+AddPizza<-function(x){
+  x<-c(x,"pizza")
+}
 # Add pizza to every meal!
+AddPizza(meals)
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,22 +1,25 @@
 # Exercise 2: Creating data frames
 
 # Create a vector of the number of points the Seahawks have scored in each game this season (google "Seahawks")
-
+game.scores<-c(3,4,733,4,33,6)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored against them in each game this season
-
+opposing.scores<-c(4,2,55,6,64,33)
 
 # Combine your two vectors into a dataframe
-
+scores.spread<-data.frame("gameScores"=game.scores,"opposingScores"=opposing.scores)
 
 # Create a new column "diff" that is the difference in points
-
+scores.spread$diff<-game.scores-opposing.scores
 
 # Create a new column "won" which is TRUE if the Seahawks wom
-
+scores.spread$won<-scores.spread[["diff"]]>0
 
 # Create a vector of the opponents
-
+opponents<-c("The miami heat","The boston whitesocks",
+             "The Canucks","Team USA","The maine marauders",
+             "The washington redskins")
 
 # Assign your dataframe rownames of their opponents
 
+scores.spread$opponents<-opponents
